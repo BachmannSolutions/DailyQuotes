87e54c2eea6da189ca827f5880a707cad4abbaf3
BachmannSolutions <BachmannSolutions@gmail.com>
2021-07-06 20:48:13 -0400

    added MENU to options for user

--- program.py
+++ program.py
@@ -6,6 +6,7 @@ quotes_file = open("quotes_file.txt", encoding="utf-8")
 data = quotes_file.read()
 quotes_file.close()
 
+menu = "\t**Enter YES to view your daily quote**\n\t**Enter NO to quit**\n"
 
 #quote = re.compile(r"""
 #    ^(?P<quote>([-\w ]*)\t # Quote
@@ -27,8 +28,7 @@ quotes_file.close()
 
 
 print("Welcome to your Daily Quote Generator!")
-print("\t**Enter YES to view your daily quote**")
-print("\t**Enter NO to quit**\n")
+print(menu)
 
 user_response_yes_count = 0
 
@@ -36,23 +36,23 @@ while True:
     user_response = input("Would you like to see your quote of the day?:  ")
     user_response = user_response.upper()
 
-# TODO: look into  this not adding as expected
-
     if user_response == "NO":
         print("\nThanks for visiting! Be sure to come back tomorrow for your new quote!\n")
         sys.exit()
 
-#TODO: Where do I add this to direct user to QUIT or YES
-#        raise ValueError("Be sure to enter QUIT to quit or YES to view quote of the day")
+    elif user_response == "MENU":
+        print("\n",menu)
 
-    if user_response == "YES":
+#TODO: Where do I add this to direct user to MENU or QUIT or YES
+#        raise ValueError("Be sure to enter: YES to view quote of the day; NO to quit; or MENU for menu ")
+
+    elif user_response == "YES":
         user_response_yes_count += 1
         if user_response_yes_count <= 2:
-            print("\nYour quote of the day is:  {}".format(data))
+            print("\n\tYour quote of the day is:  {}".format(data))
             print("")
             user_response_yes_count += 1
         # If user already got a quote for the day, ask to come back again tomorrow
         else:
-            print("Sorry. You have to wait until tomorrow to recieve your next quote. \n")
+            print("\n\tSorry. You have to wait until tomorrow to recieve your next quote. \n")
             sys.exit()
-
