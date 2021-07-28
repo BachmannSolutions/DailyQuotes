85e9a05e446a1dd8b799b3d98150d5beb54a1357
BachmannSolutions <BachmannSolutions@gmail.com>
2021-07-27 20:07:12 -0400

    Corrected code to work with directory

--- README.md
+++ README.md
@@ -6,8 +6,7 @@ The intention of this app is to enable the user to view a new quote daily.
 ## Code Louisville Project Requirements met
 1. Implement a “master loop” console application where the user can repeatedly enter commands/perform actions, including choosing to exit the program
 2. Read data from an external file, such as text, JSON, CSV, etc and use that data in your application
-3. [not utilized yet] Create a dictionary or list, populate it with several values, retrieve at least one value, and use it in your program
-4. [not utilized yet] Calculate and display data based on an external factor (ex: get the current date, and display how many days remaining until some event)
+3. Create a dictionary or list, populate it with several values, retrieve at least one value, and use it in your program
 
 ## Features to be added later
 - Connect to an external/3rd party API and read data into app to allow auto-generation of quotes instead of manually loading each to .txt file
--- program.py
+++ program.py
@@ -1,30 +1,16 @@
-import re
 import sys
-# import random
+import random
 
-quotes_file = open("quotes_file.txt", encoding="utf-8")
-data = quotes_file.read()
-quotes_file.close()
+quotes = []
+
+with open("quotes_file.txt", 'r') as text_file:
+    for line in text_file:
+        items = line.split('"')
+        quote = items[1]
+        quotes.append(quote)
+
 
 menu = "MENU:\tEnter YES to view your daily quote\n\tEnter NO to quit\n"
-
-#quote = re.compile(r"""
-#    ^(?P<quote>([-\w ]*)\t # Quote
-#    (?P<name>(?P<last>[-\w ]*),\s(?P<first>[-\w ]+))$ # First Last Name
-#""", re.X|re.MULTILINE)
-
-
-#def quote(quote):
-#    # open file
-#    with open("quotes_file.txt", "r") as file:
-#        # read thing to file
-#        file.read(thing+"\n")
-
-#def show():
-    # open file
-#    with open("quotes_file.txt") as file:
-#        for line in file:
-#            print(line)
 
 
 print("\n","*"*41,"\n * Welcome to your Daily Quote Generator *\n","*"*41)
@@ -37,23 +23,23 @@ while True:
     user_response = user_response.upper()
 
     if user_response == "NO":
-        print("\nThanks for visiting! Be sure to come back tomorrow for your new quote!\n")
+        print("\nThanks for visiting! Be sure to come back tomorrow for more quotes!\n")
         sys.exit()
 
     elif user_response == "MENU":
         print("\n",menu)
 
-#TODO: Where do I add this to direct user to MENU or QUIT or YES
-#        raise ValueError("Be sure to enter: YES to view quote of the day; NO to quit; or MENU for menu ")
-
     elif user_response == "YES":
         user_response_yes_count += 1
         # If has not recieved quote for the day, quote becomes available
         if user_response_yes_count <= 2:
-            print("\n\tYour quote of the day is:  {}".format(data))
+            quote_idx = random.randint(0,len(quotes)-1)
+            print("\n\tYour quote of the day is:  {}".format(quotes[quote_idx]))
             print("")
             user_response_yes_count += 1
         # If user already got a quote for the day, ask to come back again tomorrow
         else:
             print("\n\tSorry. You have to wait until tomorrow to recieve your next quote. \n")
             sys.exit()
+    else:
+        print("Be sure to enter: YES to view quote of the day; NO to quit; or MENU for menu ")
